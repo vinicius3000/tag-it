78fba4356145de7ab5dce6a7da2a272e2275d965
Vinicius Andreatta <viniciusandreatta@hotmail.com>
2013-09-10 02:44:03 -0300

    Help for non-logged users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,10 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+rescue_from  ActiveRecord::RecordNotFound, :with => :rescue_not_found  
+  
+def rescue_not_found  
+  flash[:error] = "A pagina que voce tentou acessar nao existe ou este projeto nao pertence a voce!"  
+  redirect_to "/"  
+end  
 end
