e3e2d6a82cafd5c93d2e856c0df3f1d7e71b63bc
Vinicius Andreatta <viniciusandreatta@hotmail.com>
2013-10-30 01:46:07 -0200

    End keyword

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -231,3 +231,5 @@ Devise.setup do |config|
   # config.omniauth_path_prefix = "/my_engine/users/auth"
 
 endconfig.secret_key = 'a533455c543a749d9ebe91fdfbed46f7235b36ba7464acc361f953a8fb6d5cfc22a461457df1a5237cee864d556e271690d9b9685ab96ce14f23ef097a1fa100'
+
+end
